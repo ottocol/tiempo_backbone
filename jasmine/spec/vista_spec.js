e278c7c6205ba3da631c56f407a78d8db29fd9c3
otto <otto@dccia.ua.es>
2015-02-18 18:47:13 +0100

    maqueados comentarios

--- jasmine/spec/vista_spec.js
+++ jasmine/spec/vista_spec.js
@@ -1,15 +1,3 @@
-describe("Pruebas sobre el modelo", function(){
-    var t;
-
-    beforeEach(function(){
-    });
-
-    it ("Un modelo recién creado no tiene localidad asignada", function(){
-        t = new DatosTiempo();
-        expect(t.has("localidad")).toBeFalsy();
-    });
-
-});
 
 describe("Pruebas sobre la vista", function() {
     var vista;
@@ -20,12 +8,12 @@ describe("Pruebas sobre la vista", function() {
     it("Al hacer click sobre el botón se debería llamar a 'ver_tiempo_de'", function(){
         vista = new TiempoWidget({model: new DatosTiempo()});
         spyOn(vista, 'ver_tiempo_de');
-        //Ponemos esto tras crear el espía para que se espíe a la función que toca, si no no va
-        //visto en SO: http://stackoverflow.com/questions/7899474/spyon-a-backbone-view-method-using-jasmine
+        //Ponemos esto tras crear el espía para que se "refresquen" los manejadores de evento
+        //Por ejemplo http://stackoverflow.com/questions/7899474/spyon-a-backbone-view-method-using-jasmine
         vista.delegateEvents();
         //Si no hacemos render de la vista no se genera el HTML y por tanto no se puede hacer clic en él
         vista.render();
-        var elem = vista.$('#ver_tiempo')
+        var elem = vista.$('#ver_tiempo');
         elem.click();
         expect(vista.ver_tiempo_de).toHaveBeenCalled();
     });
@@ -35,7 +23,6 @@ describe("Pruebas sobre la vista", function() {
        vista = new TiempoWidget({model: new DatosTiempo()});
        vista.model.trigger("change:dt");
        expect(vista.renderData).toHaveBeenCalled();
-       //expect(vista.renderData.calledOnce).toBeTruthy();
     });
 
-})
+});
